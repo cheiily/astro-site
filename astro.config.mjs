c106916be3a9c5ebecd80d8590afd1ec3613f2b3
_cheily <cheiily.pm@gmail.com>
2025-06-09 02:29:53 +0200

    api for ssc notif

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,18 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "http://cheily.one",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+
   markdown: {
     shikiConfig: {
       theme: "css-variables",
     },
   },
+
+  // adapter: netlify(),
 });
